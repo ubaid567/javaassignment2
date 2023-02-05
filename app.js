a2be5774934142969242ccb806c2139489987134
ubaid567 <124505833+ubaid567@users.noreply.github.com>
2023-02-05 05:12:54 -0800

    chapter 2 3and 4 answers

---
+++ app.js
@@ -0,0 +1,93 @@
+//chapter#2 variable for string
+//question#1
+
+//var a="user name"
+//console.log(a)
+
+//question#2
+//var myname="fullname"
+//alert(myname);
+
+//question#3
+//a) Declare a JS variable, titled message.
+
+
+//question#4
+//var name="ubaid"
+//alert(name)
+//var age="17 years old"
+//alert(age)
+//var edu="web and mobile application developper"
+//alert(edu)
+
+//question#5
+//var eat="pizza \n pizz \n piz \n pi \n p"
+//alert(eat)
+
+//question#6
+//var email="ubaidyameen567@gmail.com"
+//var my="My email address is "
+//alert(  my+ email)
+
+
+//question#7
+//var myPoint ="I am trying to learn from the Book "
+//var bookName=" A smarter way to learn JavaScript "
+//alert( myPoint+bookName)
+
+//question#8
+//document.write("<h3>yah! i can write HTML content through javascipt</h3>");
+
+
+//question#8
+//var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//alert(design)
+
+
+//chapter#3 variables for numbers
+
+//queation#1
+//var age="i am 17 years old"
+//alert(age)
+
+//question#2
+//var track=" 153 "
+//alert("you have visited this site" +track+"times")
+
+//question#3
+//document.write("<h3>My birth year is 2005</h3> <h4> data type of my decleard variable is number</h4>");
+
+//question#4
+//var lang= prompt("enter your name")
+//var lang2= prompt("enter your ordered name")
+//var lang3= prompt(" enter quantity")
+//document.write( lang+ "orderd "+lang3+lang2+ "on xyz clothing store")
+
+
+
+//chapter#4  VARIABLE NAMES: LEGAL & ILLEGAL
+
+
+//question#1
+//var num=3
+//var num2=6
+//var num3=9
+//console.log(num+num2+num3)
+
+//question#2
+//legal variables:
+//var name="ubaid"
+//var collegeName=""
+//var father_name="yameen"
+//var age23="17"
+//var website="xyx cloyhrs house"
+
+//illegal variables:
+ //name="ubaid"
+//var collegeName="supprior"
+//var father name="yameen"
+//var age23"17"
+//var @website="xyz cloth house"
+
+//question#3
+//document.write("<h1>Rules for naming JS variables</h1> <p>Variable names can only contain letter,number,dollar signl and underscorces</p>")
